1e9c177cb8f933a28064df3fd9f9c6181a911bf2
maemarge <mmarg004@plattsburgh.edu>
2024-12-10 14:04:32 -0500

    Island stub

---
+++ game/locations/maeveIsland.py
@@ -0,0 +1,66 @@
+####################################################################################################
+# Imports
+####################################################################################################
+
+from game import location
+import game.config as config
+import game.display as display
+from game.events import *
+import game.items as items
+import game.combat as combat
+import game.event as event
+import game.items as item
+import random
+
+####################################################################################################
+# Events and supporting classes
+####################################################################################################
+
+####################################################################################################
+# Treasure
+####################################################################################################
+
+####################################################################################################
+# Island definition
+####################################################################################################
+
+class Island (location.Location):
+
+    def __init__ (self, x, y, w):
+        super().__init__(x, y, w)
+        self.name = "island"
+        self.symbol = 'I'
+        self.visitable = True
+        self.locations = {}
+        self.locations["beach"] = Beach_with_ship(self)
+
+        self.starting_location = self.locations["beach"]
+
+    def enter (self, ship):
+        display.announce ("arrived at an island", pause=False)
+
+
+class Beach_with_ship (location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "beach"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+        self.event_chance = 50
+        self.events.append (seagull.Seagull())
+
+    def enter (self):
+        display.announce ("arrive at the beach. Your ship is at anchor in a small bay to the south.")
+
+    def process_verb (self, verb, cmd_list, nouns):
+        if (verb == "south"):
+            display.announce ("You return to your ship.")
+            self.main_location.end_visit()
+        elif (verb == "north"):
+            display.announce ("You walk aimlessly around the island.")
+            #config.the_player.next_loc = self.main_location.locations["trees"]
+        elif (verb == "east" or verb == "west"):
+            display.announce ("You walk all the way around the island on the beach. It's not very interesting.")
+
